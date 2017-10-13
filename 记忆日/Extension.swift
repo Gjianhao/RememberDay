fb1a6318b7927196630b313045613abbf25a279c
Gjianhao <kevinbina@163.com>
2017-10-13 14:29:41 +0800

    extension文件添加

--- 记忆日/Extension.swift
+++ 记忆日/Extension.swift
@@ -19,6 +19,43 @@ extension UIView {
         get {
             return self.frame.origin.x
         }
+        set {
+            var frame = self.frame
+            frame.origin.x = newValue
+            self.frame = frame
+        }
+    }
+    
+    public var y: CGFloat {
+        get {
+            return self.frame.origin.y
+        }
+        set {
+            var frame = self.frame
+            frame.origin.y = newValue
+            self.frame = frame
+        }
     }
 
+    public var width: CGFloat {
+        get {
+            return self.frame.size.width
+        }
+        set {
+            var frame = self.frame
+            frame.size.width = newValue
+            self.frame = frame
+        }
+    }
+    
+    public var height: CGFloat {
+        get {
+            return self.frame.size.height
+        }
+        set {
+            var frame = self.frame
+            frame.size.height = newValue
+            self.frame = frame            
+        }
+    }
 }
