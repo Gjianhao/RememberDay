f8e8c2a34e93b5d8fa258e7bb31ee2aa053b2a5f
Gjianhao <kevinbina@163.com>
2017-10-14 17:22:45 +0800

    完善extension文件

--- 记忆日/Extension.swift
+++ 记忆日/Extension.swift
@@ -58,4 +58,71 @@ extension UIView {
             self.frame = frame            
         }
     }
+    /* 下 */
+    public var bottom: CGFloat {
+        get {
+            return self.frame.origin.y + self.frame.size.height
+        }
+        set {
+            var frame = self.frame
+            frame.origin.y = newValue - self.frame.size.height
+            self.frame = frame
+        }
+    }
+    // 右
+    public var right: CGFloat {
+        get {
+            return self.frame.origin.x + self.frame.size.width
+        }
+        set {
+            var frame = self.frame
+            frame.origin.x = newValue - self.frame.size.width
+            self.frame = frame
+        }
+    }
+    // 尺寸
+    public var size: CGSize {
+        get {
+            return self.frame.size
+        }
+        set {
+            var frame = self.frame
+            frame.size = newValue
+            self.frame = frame
+        }
+    }
+    // 竖直中心对齐
+    public var centerX: CGFloat {
+        get {
+            return self.center.x
+        }
+        set {
+            var center = self.center
+            center.x = newValue
+            self.center = center
+        }
+    }
+    // 水平中心对齐
+    public var centerY: CGFloat {
+        get {
+            return self.center.y
+        }
+        set {
+            var center = self.center
+            center.y = newValue
+            self.center = center
+        }
+    }
+}
+
+extension UIColor {
+    class var Main: UIColor {
+        return UIColor(red: 74/255, green: 163/255, blue: 243/255, alpha: 1.0)
+    }
+    class var Gray: UIColor {
+        return UIColor(red: 242/255, green: 243/255, blue: 248/255, alpha: 1.0)
+    }
+    class func RGB_COLOR(r:CGFloat, g:CGFloat, b:CGFloat, alpha:CGFloat) -> UIColor {
+        return UIColor(red: r/255, green: g/255, blue: b/255, alpha: alpha)
+    }
 }
